b2085c48a5b9c35cb063e5c9560a060f866e5cf0
Julio Duarte <julio.franca.017@ufrn.edu.br>
2022-10-14 03:09:56 -0300

    Adicionado modal com informações básicas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { View,StyleSheet, Text } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
+import React, {useState} from 'react';
+import { View,StyleSheet, Text, Modal } from 'react-native';
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
 import { theme } from '../theme/theme';
-
 export function Header ({navigation}){
+  const [visibleModal, setVisibleModal] = useState(false);
 
 
     return(
+        <View>
         <View style={styles.header}>
           <Text style={styles.text}> AuRelius </Text>
-          <FontAwesome5 style={styles.icon} filled name="dog" size={26} color={theme.colors.green}   />
+          <FontAwesome5 style={styles.icon} filled name="dog" size={26} color={theme.colors.green} onPress={() => setVisibleModal(true) }  />
+        </View>
 
+        <Modal
+          animationType='fade'
+          visible={visibleModal}
+          transparent={true}
+          onRequestClose={ () => setVisibleModal(false) }
+          > 
+          <View style={styles.modal_outside}> 
+          <View style={styles.modal_box}> 
+            <View style={styles.modal_header}>
+            <Text style={styles.modal_text}>Sobre</Text>
+              <FontAwesome style={styles.icon}  name="close" size={26} color={theme.colors.white} onPress={() => setVisibleModal(false) }  />
+            </View>
+            <View style={styles.modal_content}> 
+              <Text style={styles.modal_content_text}>Este aplicativo foi desenvolvido por Julio Duarte na disciplina "Desenvolvimento para dispositivos móveis" no semestre de 2022.2</Text>
+            </View>
+          </View>
+          </View>
+
+        </Modal>
       </View>
     )};
     
@@ -39,18 +60,51 @@ export function Header ({navigation}){
         icon: {
           marginRight: 10
         },
-        modal: {
+        modal_box: {
+        width: 310,
+        backgroundColor: theme.colors.green,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 0.62,
+        elevation: 10,
+        borderRadius: 20,
+        },
+        modal_content: {
+          fontSize:28,
+          padding: 10,
+      
+        },
+        modal_content_title: {
           fontSize:20,
           backgroundColor: theme.colors.green,
-          color: theme.colors.white,
-          width: 300,
-          height: 200,
-          padding: 10,
-          alignSelf: 'center',
-          justifySelf: 'center',
+          color: theme.colors.green_dark,
+          margin: 10,
         },
-        modal_box: {
-          alignItems: "center",
-          justifyContent: "center",
-        } 
+        modal_content_text: {
+          fontSize:18,
+          backgroundColor: theme.colors.green,
+          color: theme.colors.green_dark,
+          marginHorizontal: 10,
+        },
+        modal_header: {
+          flexDirection:'row',
+          padding:10,
+          height:50,
+          alignItems: 'center',
+          justifyContent:'space-between',
+        },
+        modal_text: {
+          fontSize:28,
+          color: theme.colors.white,
+          marginLeft: 10,
+        },
+        modal_outside: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }
     });
